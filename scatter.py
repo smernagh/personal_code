8c0305edd2a58ce1dce2045e5809cc1e7cb73215
Shane Mernagh <30462113+smernagh@users.noreply.github.com>
2017-09-07 10:02:51 +0100

    Portfolio
    Codes I made in python to test for or with multitudes of data sets

---
+++ scatter.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 22 15:13:12 2017
+
+@author: smernagh
+"""
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+# Create random data with numpy
+import numpy as np
+
+N = 1000
+random_x = np.random.randn(N)
+random_y = np.random.randn(N)
+
+# Create a trace
+trace = go.Scatter(
+    x = random_x,
+    y = random_y,
+    mode = 'markers'
+)
+
+data = [trace]
+
+# Plot and embed in ipython notebook!
+py.iplot(data, filename='basic-scatter')
